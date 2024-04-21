d79afad593e6f1907cc0d4d1bdab89f231e7e0ef
Kopikou <copilowa.nadezhda2015@yandex.ru>
2024-04-21 20:37:57 +0800

    base particle system

--- ParticleSystem/Form1.Designer.cs
+++ ParticleSystem/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,6 +41,14 @@
             this.picDisplay.Size = new System.Drawing.Size(776, 426);
             this.picDisplay.TabIndex = 0;
             this.picDisplay.TabStop = false;
+            this.picDisplay.Click += new System.EventHandler(this.picDisplay_Click);
+            this.picDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDisplay_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -56,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ParticleSystem/Form1.cs
+++ ParticleSystem/Form1.cs
@@ -12,9 +12,97 @@ namespace ParticleSystem
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            /*for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                particles.Add(particle);
+            }*/
+        }
+
+        private void picDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; 
+                                   
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+
+                }
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) 
+                {
+                    var particle = new ParticleColorful();
+                    particle.FromColor = Color.Purple;
+                    particle.ToColor = Color.FromArgb(0, Color.DeepPink);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                Render(g);
+            }
+
+            picDisplay.Invalidate();
+        }
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
